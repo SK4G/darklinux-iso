b86984db1b73b40024b2e5e61b3a989882e15fe9
Luiz Salazar <luizsalazar87@gmail.com>
2021-08-05 08:12:30 -0500

    change iso label

--- installation-scripts/1-build-darklinux-iso-first-time.sh
+++ installation-scripts/1-build-darklinux-iso-first-time.sh
@@ -55,6 +55,10 @@ sed -i 's/arcolinuxl/darklinux/g' $HOME/arcolinuxl-iso/installation-scripts/40-b
 sed -i 's/ArcoLinuxL/Darklinux/g' $HOME/arcolinuxl-iso/installation-scripts/40-build-the-iso-local-again.sh
 sed -i 's/xfce/awesome/g' $HOME/arcolinuxl-iso/installation-scripts/40-build-the-iso-local-again.sh
 
+#change iso label
+sed -i 's/arcolinuxl/darklinux/g' $HOME//arcolinuxl-iso/archiso/profiledef.sh
+sed -i 's/ArcoLinux/DarkLinux/g' $HOME//arcolinuxl-iso/archiso/profiledef.sh
+
 echo
 echo "################################################################## "
 tput setaf 2
